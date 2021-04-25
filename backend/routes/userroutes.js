03a055abb95c04f8b68f08a3cc6141d9cc22b069
Prabin Gyawali <pragyawali16@gmail.com>
2021-04-24 23:18:54 -0400

    Ability to change user password

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -1,17 +1,38 @@
-
 const express = require("express");
 const router = express.Router();
-const { User } = require('../models/user.js');
+const { User } = require("../models/user.js");
 const authroutes = require("./authroutes");
+const bcrypt = require("bcrypt");
 
 router.get("/", authroutes.authenticateToken, async (req, res) => {
-    try {
-        let users = await User.find({});
-        res.status(200).send({ users });
-    } catch (err) {
-        console.log(err.stack);
-    }
-    
+	try {
+		let users = await User.find({});
+		res.status(200).send({ users });
+	} catch (err) {
+		console.log(err.stack);
+	}
+});
+
+//register function
+router.put("/change_password", authroutes.authenticateToken, async (req, res) => {
+	try {
+		const { old_password, new_password } = req.body;
+        const user = await User.findById(req.user.id);
+		if (!(await bcrypt.compare(old_password, user.password))) res.status(500).send({ msg: "Password doesnot match" });
+
+		const salt = await bcrypt.genSalt();
+		const hashedPassword = await bcrypt.hash(new_password, salt);
+
+		await User.findByIdAndUpdate(req.user.id, { $set: { password: hashedPassword } }, function (err, result) {
+			if (err) {
+				res.status(500).send({ msg: err });
+			} else {
+				res.status(200).send({ msg: "Password Updated Successfully" });
+			}
+		});
+	} catch (err) {
+		res.status(500).send({ msg: err.stack });
+	}
 });
 
 module.exports = router;
