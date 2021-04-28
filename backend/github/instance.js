db649a464df0fac6d8bfdedba5399c0fa9acbc8e
popcornn95 <danishdavis@hotmail.com>
2021-04-27 22:31:05 -0400

    removed the preview prop from the octokit

--- backend/github/instance.js
+++ backend/github/instance.js
@@ -2,8 +2,7 @@ const { Octokit } = require("@octokit/rest");
 
 const octokit = (token) => {
     return new Octokit({
-        auth: token,
-        previews: ['jean-grey', 'symmetra']
+        auth: token
     });
 }
 
