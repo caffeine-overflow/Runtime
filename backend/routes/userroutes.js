4478a298ad80532a8624efa764b6d957991717ef
Prabin Gyawali <pragyawali16@gmail.com>
2021-05-09 21:09:26 -0400

    Adds client id for user when registering

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -170,6 +170,7 @@ router.post("/create", authroutes.authAdmin, async (req, res) => {
 			image: image,
 			first_login: true,
 			git_token: null,
+			client_id: req.user.client_id,
 			role: "member"
 		});
 
