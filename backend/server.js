8213135e4876c23843ab368d24515fa7ffb0e269
popcornn95 <danishdavis@hotmail.com>
2021-06-05 23:17:31 -0400

    updated package and server

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,9 @@ const sprintroutes = require('./routes/sprintroutes');
 const userstoryroutes = require('./routes/userstoryroutes');
 const gitRoutes = require('./github/gitRoutes');
 const gitauthroutes = require('./github/authroutes');
-const logger = require('./utils/logger');
 const path = require('path');
 
-//mongo db connection
-mongoose.connect(db_connection, { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    return next();
-});
-
 //middlewares
-app.use(express.static('public'));
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
@@ -38,6 +25,28 @@ app.use('/api/users', userroutes);
 app.use('/api/projects', projectroutes);
 app.use('/api/sprints', sprintroutes);
 app.use('/api/userstories', userstoryroutes);
+
+app.enable("trust proxy");
+app.use((req, res, next) => {
+    req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+});
+
+app.use(express.static('public'));
+app.get("/*", (request, response) => {
+    response.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+//mongo db connection
+mongoose.connect(db_connection, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    return next();
+});
+
 
 app.use((error, req, res, next) => {
     return res.status(error.status).send({ msg: error.message });
