1043ae649cde30e45df03b7ab4d8ce35aed31095
Prabin Gyawali <pragyawali16@gmail.com>
2021-06-14 07:00:41 -0400

    Removed console log

--- backend/routes/authroutes.js
+++ backend/routes/authroutes.js
@@ -161,7 +161,7 @@ router.post("/login", async (req, res, next) => {
             //create the json web tokens
             const userToken = { id: user._id, email: email, firstname: user.firstname, lastname: user.lastname, role: user.role, invitationAccepted: user.invitation_accepted };
             const access_token = jwt.sign(userToken, token_secret);
-            console.log(user)
+
             return res.status(200).send(
                 {
                     access_token,
