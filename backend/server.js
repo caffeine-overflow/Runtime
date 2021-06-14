2e53a97c800d456f12aa5d704e55e794831cfc8a
popcornn95 <danishdavis@hotmail.com>
2021-06-13 20:58:38 -0400

    website for the app

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,17 @@ const path = require('path');
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+// app.enable("trust proxy");
+// app.use((req, res, next) => {
+//     req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+// });
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    return next();
+});
 
 //declare routes
 app.use('/auth', authroutes.router);
@@ -26,27 +37,19 @@ app.use('/api/projects', projectroutes);
 app.use('/api/sprints', sprintroutes);
 app.use('/api/userstories', userstoryroutes);
 
-app.enable("trust proxy");
-app.use((req, res, next) => {
-    req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+app.use(express.static(path.join(__dirname, "website")));
+app.get("/home", (request, response) => {
+    console.log(request.url)
+    response.sendFile(path.join(__dirname, "website/index.html"));
 });
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/*", (request, response) => {
-    response.sendFile(path.join(__dirname, "public/index.html"));
+    response.sendFile(path.join(__dirname, "public/index.html"))
 });
 
 //mongo db connection
 mongoose.connect(db_connection, { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    return next();
-});
-
 
 app.use((error, req, res, next) => {
     return res.status(error.status).send({ msg: error.message });
