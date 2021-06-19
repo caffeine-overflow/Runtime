8248448e1db0bb2b24b4a345beff086c237d14c7
popcornn95 <danishdavis@hotmail.com>
2021-06-18 23:10:41 -0400

    initial setup for socket io

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const http = require("http");
 const app = express();
-const { db_connection, port } = require("./config");
+const { db_connection } = require("./config");
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+let server = http.createServer(app);
+const socketIO = require('socket.io');
+let io = socketIO(server);
 const userroutes = require('./routes/userroutes');
 const authroutes = require('./routes/authroutes');
 const projectroutes = require('./routes/projectroutes');
@@ -10,7 +15,6 @@ const sprintroutes = require('./routes/sprintroutes');
 const userstoryroutes = require('./routes/userstoryroutes');
 const gitRoutes = require('./github/gitRoutes');
 const gitauthroutes = require('./github/authroutes');
-const path = require('path');
 
 //middlewares
 app.use(express.json({ limit: '2mb' }));
@@ -37,16 +41,21 @@ app.use('/api/projects', projectroutes);
 app.use('/api/sprints', sprintroutes);
 app.use('/api/userstories', userstoryroutes);
 
+//entry for the website
 app.use(express.static(path.join(__dirname, "website")));
 app.get("/home", (request, response) => {
     console.log(request.url)
     response.sendFile(path.join(__dirname, "website/index.html"));
 });
 
+//entry to the app
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/*", (request, response) => {
     response.sendFile(path.join(__dirname, "public/index.html"))
 });
+
+//sockets
+io.on('connection', (socket) => console.log('new connection established'));
 
 //mongo db connection
 mongoose.connect(db_connection, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -56,5 +65,5 @@ app.use((error, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
