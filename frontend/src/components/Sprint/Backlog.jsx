fdc10619ee0d42f490dda236937543cd0adcdcc7
popcornn95 <danishdavis@hotmail.com>
2021-09-18 12:19:27 -0400

    added loader to the backlogs page

--- frontend/src/components/Sprint/Backlog.jsx
+++ frontend/src/components/Sprint/Backlog.jsx
@@ -1,5 +1,6 @@
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import Loader from 'react-loader-spinner';
 import { Table, IconButton, Icon } from 'rsuite';
 import NotFound from "../NotFound";
 import NoBacklog from "../../assets/nobacklog.svg";
@@ -62,81 +63,92 @@ export default function Backlog(props) {
     return (
         <div>
             {
-                !loading && backlogs.length === 0 ?
-                    <NotFound
-                        image={NoBacklog}
-                        msg="So empty!"
-                    />
-                    :
-                    <div style={{ margin: '50px' }}>
-                        <Table
-                            height={700}
-                            rowHeight={60}
-                            data={backlogs}
-                            rowKey={rowKey}
-                            expandedRowKeys={expandedRowKeys}
-                            rowExpandedHeight={400}
-                            renderRowExpanded={rowData => {
-                                return (
-                                    <Editor
-                                        disabled={true}
-                                        value={rowData.description}
-                                    />
-                                );
-                            }}
-                        >
-                            <Table.Column width={100} align="center">
-                                <Table.HeaderCell style={styles.header}>#</Table.HeaderCell>
-                                <ExpandCell
-                                    dataKey="id"
-                                    expandedRowKeys={expandedRowKeys}
-                                    onChange={handleExpanded}
-                                />
-                            </Table.Column>
-
-                            <Table.Column width={200}>
-                                <Table.HeaderCell style={styles.header}>Identifier</Table.HeaderCell>
-                                <Table.Cell dataKey="identifier" />
-                            </Table.Column>
-
-                            <Table.Column width={400}>
-                                <Table.HeaderCell style={styles.header}>Title</Table.HeaderCell>
-                                <Table.Cell dataKey="title" />
-                            </Table.Column>
-
-                            <Table.Column width={200}>
-                                <Table.HeaderCell style={styles.header}>Created On</Table.HeaderCell>
-                                <Table.Cell dataKey="created_at" />
-                            </Table.Column>
-
-                            <Table.Column width={250}>
-                                <Table.HeaderCell style={styles.header}>Created By</Table.HeaderCell>
-                                <Table.Cell>
-                                    {rowData => `${rowData.created_by.firstname} ${rowData.created_by.lastname}`}
-                                </Table.Cell>
-                            </Table.Column>
-
-                            <Table.Column width={200} fixed="right">
-                                <Table.HeaderCell style={styles.header}>Action</Table.HeaderCell>
-                                <Table.Cell>
-                                    {rowData => {
-                                        return (
-                                            <p
-                                                style={{ color: '#134069', textDecoration: 'underline', cursor: 'pointer' }}
-                                                onClick={() => {
-                                                    if (!!!props.acitveSprint) return;
-                                                    moveToActiveSprint(rowData);
-                                                }}
-                                            >
-                                                Move To Active Sprint
-                                            </p>
-                                        );
-                                    }}
-                                </Table.Cell>
-                            </Table.Column>
-
-                        </Table>
+                loading ?
+                    <div style={{ height: '80vh' }}>
+                        <Loader
+                            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}
+                            type="ThreeDots"
+                            color="#134069"
+                            height={50}
+                            width={50}
+                        />
                     </div>
+
+                    : (backlogs.length === 0 ?
+                        <NotFound
+                            image={NoBacklog}
+                            msg="So empty!"
+                        />
+                        :
+                        <div style={{ margin: '50px' }}>
+                            <Table
+                                height={700}
+                                rowHeight={60}
+                                data={backlogs}
+                                rowKey={rowKey}
+                                expandedRowKeys={expandedRowKeys}
+                                rowExpandedHeight={400}
+                                renderRowExpanded={rowData => {
+                                    return (
+                                        <Editor
+                                            disabled={true}
+                                            value={rowData.description}
+                                        />
+                                    );
+                                }}
+                            >
+                                <Table.Column width={100} align="center">
+                                    <Table.HeaderCell style={styles.header}>#</Table.HeaderCell>
+                                    <ExpandCell
+                                        dataKey="id"
+                                        expandedRowKeys={expandedRowKeys}
+                                        onChange={handleExpanded}
+                                    />
+                                </Table.Column>
+
+                                <Table.Column width={200}>
+                                    <Table.HeaderCell style={styles.header}>Identifier</Table.HeaderCell>
+                                    <Table.Cell dataKey="identifier" />
+                                </Table.Column>
+
+                                <Table.Column width={400}>
+                                    <Table.HeaderCell style={styles.header}>Title</Table.HeaderCell>
+                                    <Table.Cell dataKey="title" />
+                                </Table.Column>
+
+                                <Table.Column width={200}>
+                                    <Table.HeaderCell style={styles.header}>Created On</Table.HeaderCell>
+                                    <Table.Cell dataKey="created_at" />
+                                </Table.Column>
+
+                                <Table.Column width={250}>
+                                    <Table.HeaderCell style={styles.header}>Created By</Table.HeaderCell>
+                                    <Table.Cell>
+                                        {rowData => `${rowData.created_by.firstname} ${rowData.created_by.lastname}`}
+                                    </Table.Cell>
+                                </Table.Column>
+
+                                <Table.Column width={200} fixed="right">
+                                    <Table.HeaderCell style={styles.header}>Action</Table.HeaderCell>
+                                    <Table.Cell>
+                                        {rowData => {
+                                            return (
+                                                <p
+                                                    style={{ color: '#134069', textDecoration: 'underline', cursor: 'pointer' }}
+                                                    onClick={() => {
+                                                        if (!!!props.acitveSprint) return;
+                                                        moveToActiveSprint(rowData);
+                                                    }}
+                                                >
+                                                    Move To Active Sprint
+                                                </p>
+                                            );
+                                        }}
+                                    </Table.Cell>
+                                </Table.Column>
+
+                            </Table>
+                        </div>)
             }
         </div>
     )
